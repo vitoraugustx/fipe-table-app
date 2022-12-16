755f41a5e7d101bc54523e233bd1b67d672e2b2a
Vitor <vitor.arp2010@gmail.com>
2022-12-16 17:14:03 -0300

    Comentários no código e pequenas adaptações

--- public/js/script.js
+++ public/js/script.js
@@ -209,34 +209,41 @@ function search(type_form, brand_form, model_form, year_form, result_div, type){
 
 // Função para pegar o preço do veículo
 async function getVehiclePrice(type_form, model_form, year_form, result_div, compare = false){
-    var value = type_form.options[type_form.selectedIndex].value;
-    var model_number = model_form.options[model_form.selectedIndex].id;
-    var brand_number = model_form.options[model_form.selectedIndex].value;
-    var year_number = year_form.options[year_form.selectedIndex].id;
-    let data
-    if (value == "car"){
-      const response = await fetch(FIPE_URL + "/v1/carros/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-      data = await response.json()
+  // Pegando os valores dos forms selecionados
+  var value = type_form.options[type_form.selectedIndex].value;
+  var model_number = model_form.options[model_form.selectedIndex].id;
+  var brand_number = model_form.options[model_form.selectedIndex].value;
+  var year_number = year_form.options[year_form.selectedIndex].id;
+  let data
+  // Verificando qual categoria foi selecionada e fazendo a requisição para a API
+  if (value == "car"){
+    const response = await fetch(FIPE_URL + "/v1/carros/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
+    data = await response.json()
 
-    } else if (value == "motorcycle"){
-      const response = await fetch(FIPE_URL + "/v1/motos/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-      data = await response.json()
+  } else if (value == "motorcycle"){
+    const response = await fetch(FIPE_URL + "/v1/motos/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
+    data = await response.json()
 
-    } else if (value == "truck"){
-      const response = await fetch(FIPE_URL + "/v1/caminhoes/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-      data = await response.json()
+  } else if (value == "truck"){
+    const response = await fetch(FIPE_URL + "/v1/caminhoes/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
+    data = await response.json()
 
-    } else {
-      return console.log("error")
-    }
-    renderPrice(data, result_div, compare)
-    result_div.style.display = 'block';
-    window.location.href = "#" + result_div.id;
+  } else {
+    return console.log("error")
+  }
+  // Renderiza o preço do veículo na tela
+  renderPrice(data, result_div, compare)
+  // Mostra o resultado
+  result_div.style.display = 'block';
+  // Faz o scroll para o resultado
+  window.location.href = "#" + result_div.id;
 }
 
+// Função para renderizar o preço do veículo na tela
 function renderPrice(data, result_div, compare = false){
   result_div.innerHTML = '';
   let list = '';
+  // Pega os dados da resposta da requisição passada como parâmetro
   price = data['Valor']
   brand = data['Marca']
   model = data['Modelo']
@@ -244,6 +251,7 @@ function renderPrice(data, result_div, compare = false){
   fuel_type = data['Combustivel']
   fipe_code = data['CodigoFipe']
   month_reference = data['MesReferencia']
+  // Utiliza listagem dinâmica para renderizar os dados na tela no formato de tabela
   list += `<br><br><h4>Informações do veículo</h4><br><table class="table table-bordered table-dark table-responsive">
   <tbody>
     <tr>
@@ -277,50 +285,60 @@ function renderPrice(data, result_div, compare = false){
   </tbody>
   </table>`
   result_div.innerHTML += list;
+  // Se o parâmetro compare for false, chama a função para abrir uma nova aba com as imagens do veículo
+  // Para comparações, não foi utilizada a função de abrir abas com imagens dos veículos
   if (!compare){
     openNewTab(brand, model, year, price)
   }
 }
 
+// Função para abrir uma nova aba com as imagens do veículo
 async function openNewTab(brand, model, year){
+  // Monta a url para a pesquisa no google imagens
   var url = "https://www.google.com/search?q=" + brand + "+" + model + "+" + year + "&hl=pt_br&site=imghp&tbm=isch"
   window.open(url, '', 'width=700,height=500')
 }
 
+// Função para pegar o preço de todos os anos do veículo
 async function getAllYearsPrices(data, type_form, model_form){
-    let year_array = []
-    let aux_price_array = []
-    let price_array = []
-    let year_price = []
-    for(let i = 0; i < data.length; i++){
-      year_array.push(data[i].nome.slice(0,4))
-      aux_price_array.push(await getPrice(data[i].codigo, type_form, model_form))
-    }
-    // Retira os valores que não são numéricos
-    for (let i = 0; i < aux_price_array.length; i++){
-      price_array.push(aux_price_array[i].replace('R$ ','').replaceAll('.','').replaceAll(',','.'))
-    }
-    // Convertendo para float
-    price_array = price_array.map(parseFloat)
-    // Cria um array de objetos com o ano e o preço
-    for (let i = 0; i < year_array.length; i++){
-      year_price = year_price.concat({year: year_array[i], price: price_array[i]})
-    }
-    // Ordena o array de acordo com o ano
-    year_price.sort((a, b) => parseFloat(a.year) - parseFloat(b.year));
-    return year_price
+  // Cria arrays para armazenar os anos e os preços
+  let year_array = []
+  let aux_price_array = []
+  let price_array = []
+  let year_price = []
+  // Pega os anos e os preços de cada ano
+  for(let i = 0; i < data.length; i++){
+    year_array.push(data[i].nome.slice(0,4))
+    aux_price_array.push(await getPrice(data[i].codigo, type_form, model_form))
+  }
+  // Retira os valores que não são numéricos
+  for (let i = 0; i < aux_price_array.length; i++){
+    price_array.push(aux_price_array[i].replace('R$ ','').replaceAll('.','').replaceAll(',','.'))
+  }
+  // Convertendo para float
+  price_array = price_array.map(parseFloat)
+  // Cria um array de objetos com o ano e o preço
+  for (let i = 0; i < year_array.length; i++){
+    year_price = year_price.concat({year: year_array[i], price: price_array[i]})
+  }
+  // Ordena o array de acordo com o ano
+  year_price.sort((a, b) => parseFloat(a.year) - parseFloat(b.year));
+  return year_price
 } 
 
+// Função para plotar o gráfico utilizando a biblioteca plotly JS
 function plotGraph(year_price){
-  console.log(year_price)
+  // Cria arrays para armazenar os anos e os preços
   var year_array = []
   var price_array = []
+
+  // Pega os anos e os preços de cada ano
   for (let i = 0; i < year_price.length; i++){
     year_array.push(year_price[i]['year'])
     price_array.push(year_price[i]['price'])
   }
 
-  // Define the data
+  // Configurações do gráfico
   var data = [{
     x: year_array,
     y: price_array,
@@ -328,7 +346,7 @@ function plotGraph(year_price){
     type: 'bar',
     marker: {color: 'rgb(55, 83, 109)'}
   }];
-  // Define the plot layout
+  // Define o título do gráfico e os títulos dos eixos
   var layout = {
     title: 'Preço do modelo de acordo com o ano de fabricação',
     xaxis: {
@@ -342,33 +360,39 @@ function plotGraph(year_price){
       tickprefix: 'R$',
     }
   };
-  // Display the plot
+  // Mostra o gráfico
   Plotly.newPlot('plot', data, layout);
+  // Scroll para a área do gráfico
   window.location.href='#plot';
 }
 
+// Função para pegar o preço do veículo
 async function getPrice(year_number, type_form, model_form){
+  // Valores dos forms selecionados
   var value = type_form.options[type_form.selectedIndex].value;
   var model_number = model_form.options[model_form.selectedIndex].id;
   var brand_number = model_form.options[model_form.selectedIndex].value;
+  let data
+  // Faz a requisição para a API e recebe como retorno todas as informações do veículo, mas apenas o valor é utilizado
   if (value == "car"){
     const response = await fetch(FIPE_URL + "/v1/carros/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-    const data = await response.json()
-    return data['Valor']
+    data = await response.json()
   } else if (value == "motorcycle"){
     const response = await fetch(FIPE_URL + "/v1/motos/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-    const data = await response.json()
-    return data['Valor']
+    data = await response.json()
   } else if (value == "truck"){
     const response = await fetch(FIPE_URL + "/v1/caminhoes/marcas/" + brand_number + "/modelos/" + model_number + "/anos/" + year_number)
-    const data = await response.json()
-    return data['Valor']
+    data = await response.json()
   } else {
     console.log("error")
   }
+  // retorna o valor do veículo
+  return data['Valor']
 }
 
+// Função do botão de comparação
 function search_compare(type_form_left, type_form_right, brand_form_left, brand_form_right, model_form_left, model_form_right, year_form_left, year_form_right, result_left, result_right){
+  // Valores dos forms selecionados
   var type_number_left = type_form_left.options[type_form_left.selectedIndex].value;
   var type_number_right = type_form_right.options[type_form_right.selectedIndex].value;
   var brand_number_left = brand_form_left.options[brand_form_left.selectedIndex].value;
@@ -377,9 +401,11 @@ function search_compare(type_form_left, type_form_right, brand_form_left, brand_
   var model_number_right = model_form_right.options[model_form_right.selectedIndex].value;
   var year_number_left = year_form_left.options[year_form_left.selectedIndex].value;
   var year_number_right = year_form_right.options[year_form_right.selectedIndex].value;
+  // Verifica se todos os campos foram selecionados
   if (type_number_left == "0" || brand_number_left == "0" || model_number_left == "0" || year_number_left == "0" || type_number_right == "0" || brand_number_right == "0" || model_number_right == "0" || year_number_right == "0"){
     return alert("Selecione todos os campos para realizar a comparação")
   } else {
+    // Chama a função para pegar o preço dos veículos e renderizar na tela
     getVehiclePrice(type_form_left, model_form_left, year_form_left, result_left, true)
     getVehiclePrice(type_form_right, model_form_right, year_form_right, result_right, true)
   }
